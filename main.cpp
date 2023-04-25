81c7f3ec48ac8b2e5e1fe927e359bb6fa2c59c49
CaffeineCreature <chrispearson9@gmail.com>
2023-04-25 14:56:18 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,179 +1,107 @@
 #include <iostream>
-#include <string>
-#include <random>
-#include <cmath>
-#include <ctime>
-#include <queue>
+#include <pthread.h>
 #include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
 
-// Event states
-constexpr uint32_t EVENT_ARRIVAL = 1;
-constexpr uint32_t EVENT_CPU_COMPLETE = 2;
-constexpr uint32_t EVENT_DISK_COMPLETE = 4;
-constexpr uint32_t EVENT_EXIT = 8;
+pthread_mutex_t dealerMutex;
+pthread_mutex_t playerMutex;
+std::vector<pthread_cond_t> playerTurns(6);
 
-// Process event to report state change
-struct event {
-    double time;
-    uint32_t pid;
-    uint32_t eventType;
+struct gameData {
+    int round;
+    std::vector<int> cards;
 };
 
-struct process{
-    uint32_t pid;
-    double arrivalTime;
-    double completionTime;
+struct playerData {
+    int id;
+    gameData* game;
 };
 
-struct stats{
-    double turnaround;
-    double throughput;
-    double cpuUtilization;
-    double diskUtilization;
-    double readyQueueWaiting;
-    double diskQueueWaiting;
-};
+void* playerThread(void* par){
+    auto pData = (playerData *)par;
+    const int id = pData->id;
+    const auto pGame = pData->game;
 
-// poissonDelay Generates a random delay based on the Poisson distribution
-// input: lambda rate
-// output: delay in seconds
-inline double poissonDelay(const double lambda){
-    // Adapted to fit the edge cases of std::rand() value. Absolute pain to debug :(
-    return -log(1.0-(((double)std::rand()+1)/((double)RAND_MAX+2)))/lambda;
-}
+    while(pGame->round < 1){
+        //pthread_mutex_lock(&playerMutex);
+        if(pGame->round % 6 == id){
+            pthread_mutex_lock(&dealerMutex);
 
-// runSimulation Runs the simulated system until n cycles are completed
-// input: n number of processes to complete, lambda arrival rate, out .csv output destination
-void runSimulation(const double lambda, const double cpuMean, const double diskMean){
-    double simTime = 0.0;
-    const int n = 10000;
-    int completionCount = 0;
-    process nextArrival = {0,0.0,0.0};
-    //std::vector<event> eventLog;
-    stats simStats = stats{0,0,0,0,0,0};
+            printf("Player%i is Dealer\n", id);
 
-    // CPU state
-    std::queue<process> cpuQueue;
-    double cpuCompleteTime = 0.0;
-
-    // Disc state
-    std::queue<process> diskQueue;
-    double diskCompleteTime = 0.0;
-
-    // Time step is set based on the next event completion
-    while(completionCount < n) {
-        // Simulate arrival rate
-        if(nextArrival.arrivalTime <= simTime){
-            if(cpuQueue.empty())
-                cpuCompleteTime = simTime + poissonDelay(1/cpuMean);
-
-            cpuQueue.push(nextArrival);
-            //eventLog.push_back(event{nextArrival.arrivalTime, nextArrival.pid,EVENT_ARRIVAL});
-
-            //std::cout << "ARIV: pid" << nextArrival.pid << "@t="<< simTime << "\n";
-            nextArrival = process{nextArrival.pid + 1,
-                                  nextArrival.arrivalTime + poissonDelay(lambda),
-                                  0.0};
-        }
-        // Simulate CPU completion
-        if(!cpuQueue.empty() && cpuCompleteTime <= simTime){
-            if(((double)std::rand()/RAND_MAX) <= 0.6){
-                // Program exit
-                completionCount++;
-                //eventLog.push_back(event{cpuCompleteTime,
-                //                         cpuQueue.front().pid,
-                //                         EVENT_EXIT});
-                //std::cout << "EXIT: pid" << cpuQueue.front().pid << "@t="<< cpuCompleteTime<< "\n";
-
-                simStats.throughput++;
-                simStats.turnaround += cpuCompleteTime - cpuQueue.front().arrivalTime;
-            } else { //Program request disk
-                if(diskQueue.empty())
-                    diskCompleteTime = simTime + poissonDelay(1/diskMean);
-                //std::cout << "QUED: pid" << cpuQueue.front().pid << "@t="<< cpuCompleteTime<< "\n";
-                diskQueue.push(cpuQueue.front());
+            for(int i = 1; i <6;i++){
+                pthread_mutex_lock(&playerMutex);
+                printf("Player%i is signaled\n", i);
+                pthread_cond_signal(&playerTurns[i]);
+                pthread_cond_wait(&playerTurns[id], &playerMutex);
+                pthread_mutex_unlock(&playerMutex);
             }
-            cpuQueue.pop();
-            if(!cpuQueue.empty())
-                cpuCompleteTime = simTime + poissonDelay(1/cpuMean);
 
-        }
-        // Simulate Disk completion
-        if(!diskQueue.empty() && diskCompleteTime <= simTime){
-            // Place in CPU queue
-            if(cpuQueue.empty())
-                cpuCompleteTime = simTime + poissonDelay(1/cpuMean);
-            cpuQueue.push(diskQueue.front());
+            pGame->round++;
 
+            pthread_mutex_unlock(&dealerMutex);
+        } else {
+            printf("Player%i is in waiting\n", id);
+            pthread_mutex_lock(&playerMutex);
+            pthread_cond_wait(&playerTurns[id],&playerMutex);
+            printf("Player%i is in play\n", id);
 
-            //std::cout << "CMPD: pid" << diskQueue.front().pid << "@t="<< diskCompleteTime<< "\n";
-            diskQueue.pop();
-            if(!diskQueue.empty())
-                diskCompleteTime = simTime + poissonDelay(1/diskMean);
+            pthread_cond_signal(&playerTurns[pGame->round%6]);
+            printf("Dealer is signaled by %i\n", id);
+            pthread_mutex_unlock(&playerMutex);
         }
-
-        // Update simTime
-        double deltaTime = simTime;
-        simTime = nextArrival.arrivalTime;
-        if(!cpuQueue.empty()) {
-            simTime = std::min(simTime, cpuCompleteTime);
-        }
-        if(!diskQueue.empty()) {
-            simTime = std::min(simTime, diskCompleteTime);
-        }
-
-        // Utilization stats
-        deltaTime = simTime-deltaTime;
-        if(!cpuQueue.empty()) {
-            simStats.cpuUtilization += deltaTime;
-        }
-        if(!diskQueue.empty()) {
-            simStats.diskUtilization += deltaTime;
-        }
-        // Accumulate queue stats
-        // Item at front of queue is being processed, not counted
-        simStats.readyQueueWaiting += deltaTime * (double)std::max((int)cpuQueue.size()-1,0);
-        simStats.diskQueueWaiting += deltaTime * (double)std::max((int)diskQueue.size()-1,0);
+        //pthread_mutex_unlock(&playerMutex);
     }
 
-    // Normalize stats
-    simStats.turnaround = simStats.turnaround/n;
-    simStats.throughput = simStats.throughput/simTime;
-    simStats.cpuUtilization = simStats.cpuUtilization/simTime;
-    simStats.diskUtilization = simStats.diskUtilization/simTime;
-    simStats.readyQueueWaiting = simStats.readyQueueWaiting/simTime;
-    simStats.diskQueueWaiting = simStats.diskQueueWaiting/simTime;
-
-    // Print result
-    std::cout << "\nLambda[" << lambda << "]\n";
-    std::cout << "Turnaround:" << simStats.turnaround << "s\n";
-    std::cout << "Throughput:" << simStats.throughput << "/s\n";
-    std::cout << "CPU:" << simStats.cpuUtilization*100 << "%\n";
-    std::cout << "Disk:" << simStats.diskUtilization*100 << "%\n";
-    std::cout << "CPU Queue:" << simStats.readyQueueWaiting << "\n";
-    std::cout << "Disk Queue:" << simStats.diskQueueWaiting << "\n";
-
-    /*//CSV Style
-    std::cout << "Lambda[" << lambda << "],";
-    std::cout << simStats.turnaround << "," << simStats.throughput<< ","<< simStats.cpuUtilization*100<< ","
-    << simStats.diskUtilization*100 << ","<< simStats.readyQueueWaiting<< ","<< simStats.diskQueueWaiting << "\n";
-     */
+    pthread_exit(0);
 }
 
-int main(int argc, char** argv) {
-    std::srand(std::time(nullptr));
-    if(argc < 4) {
-        std::cout << "---HELP---\n";
-        std::cout << "Writen by Christopher Pearson 2023\n";
-        std::cout << "CS4328\n";
-        std::cout << "Command: $simulator #lambda #CPUTime #DiskTime\n";
-        std::cout << "Example: $simulator 12 0.02 0.06\n";
-        std::cout << "----------\n";
-    } else {
-        double lambda = std::stod(argv[1]);
-        double cpuMean = std::stod(argv[2]);
-        double diskMean = std::stod(argv[3]);
-        runSimulation(lambda, cpuMean, diskMean);
+int main() {
+    //std::cout << "Hello, World!" << std::endl;
+    std::vector<pthread_t> players(6);
+    std::vector<playerData> dataSet(6);
+
+    // Game variables
+    gameData game;
+    game.round = 0;
+    game.cards = std::vector<int>(52);
+    std::iota(game.cards.begin(),game.cards.end(), 0);
+
+    pthread_mutex_init(&dealerMutex, NULL);
+    pthread_mutex_init(&playerMutex, NULL);
+
+    for(auto t: playerTurns){
+        auto err = pthread_cond_init(&t, NULL);
+        if(err) {
+            printf("COND_ERROR: %i", err);
+            return err;
+        }
     }
+
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+    for(int i = 0; i < 6; i++){
+        dataSet[i].id = i;
+        dataSet[i].game = &game;
+
+        int err = pthread_create(&players[i],&attr,playerThread,&dataSet[i]);
+        if(err){
+            std::printf("CREATE_ERROR[%i] %i\n",i,err);
+            return err;
+        }
+    }
+
+    pthread_attr_destroy(&attr);
+    for(int i = 0; i < 6; i++){
+        int err = pthread_join(players[i],NULL);
+        if(err){
+            std::printf("JOIN_ERROR[%i] %i\n",i,err);
+            return err;
+        }
+    }
+
     return 0;
 }
