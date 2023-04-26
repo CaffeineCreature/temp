aee720c4245eea43d76d58f7ce2b5f474035a46b
CaffeineCreature <chrispearson9@gmail.com>
2023-04-25 23:02:06 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ std::vector<pthread_cond_t> playerTurns(6);
 
 struct gameData {
     int round;
+    int turn;
     std::vector<int> cards;
 };
 
@@ -26,34 +27,47 @@ void* playerThread(void* par){
     const auto pGame = pData->game;
 
     while(pGame->round < 1){
-        //pthread_mutex_lock(&playerMutex);
         if(pGame->round % 6 == id){
             pthread_mutex_lock(&dealerMutex);
 
-            printf("Player%i is Dealer\n", id);
+            printf("Player%i is Dealing\n", id);
 
+
+            // Deal to players in order
             for(int i = 1; i <6;i++){
                 pthread_mutex_lock(&playerMutex);
-                printf("Player%i is signaled\n", i);
+                printf("Player%i signaled\n", i);
+                pGame->turn = i;
                 pthread_cond_signal(&playerTurns[i]);
-                pthread_cond_wait(&playerTurns[id], &playerMutex);
+                // Wait for player to finish
+                while(pGame->turn != id) {
+                    pthread_cond_wait(&playerTurns[id], &playerMutex);
+                }
+                printf("idle turn:%i\n", pGame->turn);
                 pthread_mutex_unlock(&playerMutex);
             }
 
             pGame->round++;
+            // Deal to players in order
 
             pthread_mutex_unlock(&dealerMutex);
         } else {
-            printf("Player%i is in waiting\n", id);
             pthread_mutex_lock(&playerMutex);
-            pthread_cond_wait(&playerTurns[id],&playerMutex);
-            printf("Player%i is in play\n", id);
+            printf("Player%i waiting\n", id);
+            // Wait for signal from dealer
+            while(pGame->turn != id) {
+                //printf("idle%i\n", id);
+                pthread_cond_wait(&playerTurns[id], &playerMutex);
+            }
+            printf("Player%i in play\n", id);
 
+            // Return control to dealer
+            pGame->turn = pGame->round%6;
             pthread_cond_signal(&playerTurns[pGame->round%6]);
             printf("Dealer is signaled by %i\n", id);
+
             pthread_mutex_unlock(&playerMutex);
         }
-        //pthread_mutex_unlock(&playerMutex);
     }
 
     pthread_exit(0);
@@ -69,6 +83,8 @@ int main() {
     game.round = 0;
     game.cards = std::vector<int>(52);
     std::iota(game.cards.begin(),game.cards.end(), 0);
+    game.turn = 0;
+
 
     pthread_mutex_init(&dealerMutex, NULL);
     pthread_mutex_init(&playerMutex, NULL);
